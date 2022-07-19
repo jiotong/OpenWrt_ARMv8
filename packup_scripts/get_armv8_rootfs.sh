c83ca3aaf1801dd8b082c3c9edf867df63188a27
jiotong <72918802+jiotong@users.noreply.github.com>
2022-07-20 02:11:28 +0800

    Update get_armv8_rootfs.sh

--- packup_scripts/get_armv8_rootfs.sh
+++ packup_scripts/get_armv8_rootfs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Files 标签 ID 获取自行编译的基础镜像 https://api.github.com/repos/jiotong/Actions-OpenWrt/releases/latest
-wget $(curl -s https://api.github.com/repos/jiotong/Actions-OpenWrt/releases/72343245 | grep browser_download_url | cut -d '"' -f 4)
+wget $(curl -s https://api.github.com/repos/jiotong/OpenWrt_ARMv8/releases/72345770 | grep browser_download_url | cut -d '"' -f 4)
